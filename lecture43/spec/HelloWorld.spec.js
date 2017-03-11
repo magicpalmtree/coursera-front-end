faee94689b0b9890f38bafe2df5986495b9d37ea
magicpalmtree <magicpalmtree.space@gmail.com>
2017-03-11 20:14:38 +0800

    feat:Add jasmine example. feat:Add codes of Testing Javascript with Jasmine.

---
+++ lecture43/spec/HelloWorld.spec.js
@@ -0,0 +1,12 @@
+describe("HelloWorld", function() {
+
+
+
+
+  it("Out put the string 'hello world.'", function() {
+    var result = sayHello();
+    expect(result).toEqual('hello world.');
+  });
+
+
+});
